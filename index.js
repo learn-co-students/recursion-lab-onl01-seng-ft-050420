0aea6de2dc00e8f9802be212d65ad239bbbd46a3
uharston <uharston@gmail.com>
2020-11-18 14:39:38 -0600

    Wrote reverseString function

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-// Code your solution here!
+function printString(string) {
+    if (string.length > 0) {
+        console.log(string[0])
+        return printString( string.substring(1, string.length) )
+    }
+    else {
+        return true
+    }
+}
 
+function reverseString(string) {
+    if (string !== "") {
+        return reverseString( string.substr(1) ) + string.charAt(0);
+    }
+    else {
+        return ''
+    }
+}
